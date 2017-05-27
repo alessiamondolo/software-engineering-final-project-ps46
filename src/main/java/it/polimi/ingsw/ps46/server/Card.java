6aef8eb85be0032664ca7156e04a16ef53fc1b0a
Mondolo Alessia <a.mondolo@reply.it>
2017-05-27 13:36:38 +0200

    stub of cards architecture

--- src/main/java/it/polimi/ingsw/ps46/server/Card.java
+++ src/main/java/it/polimi/ingsw/ps46/server/Card.java
@@ -1,101 +1,42 @@
 package it.polimi.ingsw.ps46.server;
 
-import java.util.HashSet;
-
-
 /**
- * Description of Card.
+ * Card is an object that represent a single card of the game.
  * 
- * @author a.mondolo
+ * @author Alessia Mondolo
  */
-public class Card {
+public abstract class Card {
+	
+	private final String cardName;
+	private final Effect immediateEffects;
+	private final Effect permanentEffects;
+	private final Resources cost;
+	
 	/**
-	 * Description of the property effects.
+	 * Build a new Card object.
 	 */
-	public HashSet<Effect> effects = new HashSet<Effect>();
-
-	/**
-	 * Description of the property idCard.
-	 */
-	public Integer idCard = Integer.valueOf(0);
-
-	/**
-	 * Description of the property period.
-	 */
-	public Integer period = Integer.valueOf(0);
-
-	/**
-	 * Description of the property type.
-	 */
-	public Object type;
-
-	/**
-	 * The constructor.
-	 */
-	public Card() {
-		super();
+	public Card(String cardName, Effect immediateEffects, Effect permanentEffects, Resources cost) {
+		this.cardName = cardName;
+		this.immediateEffects = immediateEffects;
+		this.permanentEffects = permanentEffects;
+		this.cost = cost;
+	}
+	
+	
+	public String getCardName() {
+		return cardName;
 	}
 
-	/**
-	 * Description of the method AcquireResources.
-	 */
-	public void AcquireResources() {
+	public Effect getImmediateEffects() {
+		return immediateEffects;
 	}
 
-	/**
-	 * Returns effects.
-	 * @return effects 
-	 */
-	public HashSet<Effect> getEffects() {
-		return this.effects;
+	public Effect getPermanentEffects() {
+		return permanentEffects;
 	}
 
-	/**
-	 * Returns idCard.
-	 * @return idCard 
-	 */
-	public Integer getIdCard() {
-		return this.idCard;
+	public Resources getCost() {
+		return cost;
 	}
-
-	/**
-	 * Sets a value to attribute idCard. 
-	 * @param newIdCard 
-	 */
-	public void setIdCard(Integer newIdCard) {
-		this.idCard = newIdCard;
-	}
-
-	/**
-	 * Returns period.
-	 * @return period 
-	 */
-	public Integer getPeriod() {
-		return this.period;
-	}
-
-	/**
-	 * Sets a value to attribute period. 
-	 * @param newPeriod 
-	 */
-	public void setPeriod(Integer newPeriod) {
-		this.period = newPeriod;
-	}
-
-	/**
-	 * Returns type.
-	 * @return type 
-	 */
-	public Object getType() {
-		return this.type;
-	}
-
-	/**
-	 * Sets a value to attribute type. 
-	 * @param newType 
-	 */
-	public void setType(Object newType) {
-		this.type = newType;
-	}
-
+	
 }
--- src/main/java/it/polimi/ingsw/ps46/server/CharacterCard.java
+++ src/main/java/it/polimi/ingsw/ps46/server/CharacterCard.java
@@ -1,45 +1,11 @@
 package it.polimi.ingsw.ps46.server;
 
-
-/**
- * Description of CharacterCard.
- * 
- * @author a.mondolo
- */
 public class CharacterCard extends Card {
-	/**
-	 * Description of the property requiredMoney.
-	 */
-	public Integer requiredMoney = Integer.valueOf(0);
-	
 
-	/**
-	 * The constructor.
-	 */
-	public CharacterCard() {
-		super();
-	}
-
-	/**
-	 * Description of the method acquireCard.
-	 */
-	public void acquireCard() {
-	}
-
-	/**
-	 * Returns requiredMoney.
-	 * @return requiredMoney 
-	 */
-	public Integer getRequiredMoney() {
-		return this.requiredMoney;
-	}
-
-	/**
-	 * Sets a value to attribute requiredMoney. 
-	 * @param newRequiredMoney 
-	 */
-	public void setRequiredMoney(Integer newRequiredMoney) {
-		this.requiredMoney = newRequiredMoney;
+	public CharacterCard(String cardName, Effect immediateEffects, Effect permanentEffects,
+			Resources cost) {
+		super(cardName, immediateEffects, permanentEffects, cost);
+		// TODO Auto-generated constructor stub
 	}
 
 }
