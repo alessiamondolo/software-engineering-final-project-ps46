fea92da970372f4f200a4bc202f3c62373a0c963
darksepiol <lorenzo.garbujo@mail.polimi.it>
2017-06-24 17:09:49 +0200

    Sync img foulder and updated Dashboard class

---
+++ src/main/java/it/polimi/ingsw/ps46/client/GUI/GUIView.java
@@ -0,0 +1,74 @@
+package it.polimi.ingsw.ps46.client.GUI;
+
+import java.util.Observable;
+
+import javax.swing.JFrame;
+
+import it.polimi.ingsw.ps46.server.EventAcceptor;
+import it.polimi.ingsw.ps46.server.EventMV;
+import it.polimi.ingsw.ps46.server.EventMessage;
+import it.polimi.ingsw.ps46.server.Game;
+import it.polimi.ingsw.ps46.server.View;
+
+/**
+ * A WindowClass acts as the entry point for the game User Interface by launching the 
+ * @MainWindowCLass.
+ * 
+ * @author lorenzo
+ *
+ */
+
+public class GUIView extends View {  
+	
+	private Game game;
+	private MainWindow mainWindow;
+	
+	public static void main(String[] args) {
+		new GUIView(null);
+	}
+	
+	public GUIView (Game game) {
+		this.game = game;
+		
+		//Schedule a job for the event-dispatching thread:
+		//creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
+	
+	/**
+	 *  Cascade creation of the game GUI, this method displays the main JFrame 
+	 *  (MainWindow) which will contain the Board and the Users Panels.
+	 */
+	
+	private void createAndShowGUI() {
+		// Create and set up the window.
+		mainWindow = new MainWindow();
+		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainWindow.pack();
+		mainWindow.setVisible(true);
+	}
+
+	@Override
+	public void update(Observable obs, Object obj) {
+		((EventAcceptor) obj).accept(this);
+		
+		mainWindow.update(obs, obj);
+	}
+	
+	@Override
+	public void visit(EventMessage eventMessage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void visit(EventMV eventMV) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
--- src/main/java/it/polimi/ingsw/ps46/client/GUI/PlayerArea.java
+++ src/main/java/it/polimi/ingsw/ps46/client/GUI/PlayerArea.java
@@ -3,38 +3,48 @@ package it.polimi.ingsw.ps46.client.GUI;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 import javax.swing.border.Border;
 
 public class PlayerArea extends JPanel {
 
-	public PlayerArea(Dimension playerAreaDimension) {
+	JTabbedPane dashboardArea = new JTabbedPane();
 	
-//il MainWindow launcher dovrà passare informazioni ai suoi metodi, per esempio dimensione e username dei players
+	public PlayerArea(Dimension playerAreaDimension) {
 
-	/*	PlayerDashboard player1Dash = new PlayerDashboard(); 
-	PlayerDashboard player2Dash = new PlayerDashboard();
-	PlayerDashboard player3Dash = new PlayerDashboard();
-	PlayerDashboard player4Dash = new PlayerDashboard();*/
+		
+//la Guiview dovrà passare informazioni ai suoi metodi, per esempio dimensione e username dei players
 	
 	String username = "Ciao";
 	this.setPreferredSize(playerAreaDimension);
 	Border border = BorderFactory.createLineBorder(Color.RED, 1);
 	this.setBorder(border);
 	
-	this.setLayout(new BorderLayout());
-	BoxLayout l = new BoxLayout(this, BoxLayout.Y_AXIS);
-	this.setLayout(l);
+	this.setLayout(new GridBagLayout());
+	GridBagConstraints gbc = new GridBagConstraints();
 	
-	this.add(new PlayerDashboard(username, playerAreaDimension));
-		
-/*	this.add(player1Dash);
-	this.add(player1Dash);
-	this.add(player1Dash);
-	this.add(player1Dash);*/
+	gbc.fill = GridBagConstraints.BOTH;  
+	gbc.gridwidth = 24;
+	gbc.gridheight = 8;
+	gbc.weightx = 1;
+	gbc.weighty = 1;
+	gbc.gridx = 2;
+	gbc.gridy = 4;
+	
+	dashboardArea.setBorder(border);
+	dashboardArea.addTab("Player 1", null, new PlayerDashboard(username, playerAreaDimension), "Tab 1");
+	dashboardArea.setSelectedIndex(0);
+	dashboardArea.addTab("Player 2", null, new PlayerDashboard(username, playerAreaDimension), "Tab 2");
+	dashboardArea.addTab("Player 3", null, new PlayerDashboard(username, playerAreaDimension), "Tab 3");
+	dashboardArea.addTab("Player 4", null, new PlayerDashboard(username, playerAreaDimension), "Tab 4");
+	
+	this.add(dashboardArea, gbc);
 	
 	}
 	
